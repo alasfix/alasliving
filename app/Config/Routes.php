479ce80a811f124b6969125072fe21b30eae61b6
alasfix <131499608+alasfix@users.noreply.github.com>
2024-09-17 18:36:06 +0800

    new furni
    new furni first page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/wishlist','Wishlist::index');
-$routes->get('/wishlist/(:any)','Wishlist::slug/$1');
